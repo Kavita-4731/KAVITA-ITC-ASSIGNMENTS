dc2d695172f0adb4aa50f09ee7d20a6aa15e869a
Kavita-4731 <129936023+Kavita-4731@users.noreply.github.com>
2023-05-21 23:47:45 +0530

    Add files via upload

---
+++ correctedassig1.ef.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 21 23:44:06 2023
+
+@author: kavita narayan
+"""
+
+#Question no.1
+
+b ="Python is a case sensitive language"
+idx=b.index("a")
+white_spaces=b.strip()
+
+print(b)
+print(idx)
+print(white_spaces)
